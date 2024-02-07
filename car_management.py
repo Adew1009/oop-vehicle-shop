1a763b6086bfbb7abdb7cd483f3c67a4bb48e5a6
Andrew_Dew <adew1009@gmail.com>
2024-02-07 11:01:19 -0600

    Created CarManager class and init method

--- car_management.py
+++ car_management.py
@@ -1,2 +1,25 @@
 class CarManager:
-    pass
+    all_cars = []
+    total_cars = 0
+
+    def __init__(self, id, make, model, year, mileage, services):
+        self._id = id
+        self._make = make
+        self._model = model
+        self._year = year
+        self._mileage = mileage
+        self._services = services
+        CarManager.total_cars += 1
+        CarManager.all_cars.append(self._id)
+        
+        
+
+
+my_car = CarManager(12345678,"Toyota", "Camry", 2021, 16543, ["Onstar", "Free Oil Change"])
+print(CarManager.all_cars)
+print(CarManager.total_cars)
+your_car = CarManager(13245678,"Ford", "Focus", 2019, 24325, ["Powertrain Warranty", "Free Oil Change"])
+print(CarManager.all_cars)
+print(CarManager.total_cars)
+
+
